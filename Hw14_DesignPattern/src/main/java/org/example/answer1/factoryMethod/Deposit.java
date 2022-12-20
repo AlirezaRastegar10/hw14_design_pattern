33b660f2bad478ff06c64f1783c809defdb5df42
phylix <alirezarastegar117390@gmail.com>
2022-12-20 17:12:19 +0330

    seventh commit -> create an abstract deposit class for handling FactoryMethod design pattern

---
+++ Hw14_DesignPattern/src/main/java/org/example/answer1/factoryMethod/Deposit.java
@@ -0,0 +1,20 @@
+package org.example.answer1.factoryMethod;
+
+public abstract class Deposit {
+    public abstract String getFirstname();
+    public abstract String getLastname();
+    public abstract String getDepositType();
+    public abstract Long getBalance();
+    public abstract Boolean getCheckBook();
+    public abstract Boolean getProfit();
+
+    @Override
+    public String toString() {
+        return "\n{firstname: " + this.getFirstname() + "}\n" +
+                "{lastname: " + this.getLastname() + "}\n" +
+                "{deposit type: " + this.getDepositType() + "}\n" +
+                "{balance: " + this.getBalance() + "}\n" +
+                "{checkbook: " + this.getCheckBook() + "}\n" +
+                "{profit: " + this.getProfit() + "}";
+    }
+}
